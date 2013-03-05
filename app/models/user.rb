25f77a929d2b6683f10af6cfb9a488fccb7f41ac
shanhua <shanhua2011@gmail.com>
2013-03-05 11:15:54 +0800

    用户profile

--- app/models/user.rb
+++ app/models/user.rb
@@ -63,4 +63,20 @@ class User < ActiveRecord::Base
   def nick_name
     is_guest? ? 'guest' : email
   end
+
+  def all_remaining_days
+    days = 0
+    orders.each{|o| days += o.remaining_days}
+    days
+  end
+  
+  #总截止日期
+  def all_deadline
+    (Time.now + all_remaining_days.send('days')).strftime("%Y-%m-%d")
+  end
+  
+  #拿出最后一个订单的购买时间
+  def invoice_date
+    orders.order('created_at desc').first.buy_date.strftime("%Y-%m-%d")
+  end
 end
